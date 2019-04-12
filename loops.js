64610d64264dca839218e647b16bcea1102f4b83
Dave Boehmer <daboehmer@gmail.com>
2019-04-12 03:18:56 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -17,5 +17,11 @@ function whileLoop(number) {
 }
 
 function doWhileLoop(integer) {
-  
+  function incrementVariable() {
+    i = i - 1;
+    return i;
+  }
+  do {
+    console.log("I run once regardless")
+  } while (incrementVariable() )
 }
